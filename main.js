058bf94e1553af15c9047a9faee765d4276440d4
marcus-rk <marcus.rappenborg@gmail.com>
2023-11-21 15:09:29 +0100

    Made core functionality in html, css and JS

--- main.js
+++ main.js
@@ -1,9 +1,68 @@
+const ageInputElement = document.querySelector('#age');
+const categoryElement = document.querySelector('#category');
+const keywordElement = document.querySelector('#keyword');
 const button = document.querySelector('button');
-const input = document.querySelector('input');
-const pTag = document.querySelector('p');
+const generatedContainerElement = document.querySelector('.generated-container');
+const orderedListElement = document.querySelector('ol');
+
 button.addEventListener("click", () => {
-    getGeneratedText(`The following is a list of 10 dishes you can make with these ingredients: ${input.value}`)
+    console.log("Button clicked!");
+
+    const ageString = `Generate three christmas gift ideas. Gift receiver age: ${ageInputElement.value}. `;
+    const giftTypeString = `The gift should be a ${categoryElement.value} gift.`;
+    const keywordsString = `The gift should be about: ${keywordElement.value}`;
+
+    const queryPrompt = ageString + giftTypeString + keywordsString;
+
+    getGeneratedText(queryPrompt)
         .then(generatedText => {
-            pTag.innerText = generatedText;
+            // Remove the queryPrompt from the generatedText
+            const slicedGeneratedText = generatedText.slice(queryPrompt.length, generatedText.length);
+
+            const giftIdeasStringArray = getGiftIdeas(slicedGeneratedText);
+
+            giftIdeasStringArray.forEach(giftIdeaString => {
+                const giftLiElement = getGiftLiElement(giftIdeaString);
+                renderGift(giftLiElement);
+            });
+
+            generatedContainerElement.classList.remove('hidden');
         });
+
 });
+
+function getGiftLiElement(giftIdeaString) {
+    const li = document.createElement('li');
+    li.innerText = giftIdeaString;
+
+    return li;
+}
+
+function renderGift(giftLiElement) {
+    orderedListElement.appendChild(giftLiElement);
+}
+
+function getGiftIdeas(generatedText) {
+    const giftIdeas = [];
+    let sliceStart = 0;
+
+    const firstIndex = generatedText.indexOf('1'); // generatedText often has /n in front
+
+    for (let i = firstIndex; i < generatedText.length; i++) {
+        if (generatedText[i] === '2') {
+            const firstGiftIdea = generatedText.slice(firstIndex + 2, i-1); // remove '1. ' in start
+            giftIdeas.push(firstGiftIdea);
+            sliceStart = i;
+        }
+        if (generatedText[i] === '3') {
+            const secondGiftIdea = generatedText.slice(sliceStart + 2, i-1); // remove '2. ' in start
+            giftIdeas.push(secondGiftIdea);
+
+            const thirdGiftIdea = generatedText.slice(i+2, generatedText.length); // remove '3. ' in start
+            giftIdeas.push(thirdGiftIdea);
+            break;
+        }
+    }
+
+    return giftIdeas;
+}
